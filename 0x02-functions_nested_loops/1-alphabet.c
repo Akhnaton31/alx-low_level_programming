06bef57db0d53509b4994f2417ada054d23f245e
Hossam Eldin Mostafa Mohamed <hossameldinmostafamohamedahmed@gmail.com>
2023-06-20 18:05:02 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+ * main- Entrypoint
+ * Description print lower case alphabet
+ * Return always 0
+ */
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i <= 122; 1++)
+	{
+	_putchar(i);
+	}
+	_putchar(\n);
+
+}

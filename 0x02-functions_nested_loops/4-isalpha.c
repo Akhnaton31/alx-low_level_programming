d08ad754dacc09e1f2a27e9f6c85e5dc1f796ce5
Hossam Eldin Mostafa Mohamed <hossameldinmostafamohamedahmed@gmail.com>
2023-06-21 12:08:06 -0700

    correcting 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,13 @@
-# include "main.h"
-/**
- * main-checkthecode.
- *
- *Return: Always 0.
- */
-int main(void)
-{
-	int r;
 
-	r = _islower('H');
-	_putchar(r + 'O');
-	r = _islower(r + 'o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+#include "main.h"
+
+
+/**
+* _isalpha - checks for alphabetic character
+* @c: the character to be checked
+* Return: 1 if c is a letter, 0 otherwise
+*/
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }

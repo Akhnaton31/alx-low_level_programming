ef8df50ad37447f3fff567364e80d0588c35c067
Hossam Eldin Mostafa Mohamed <hossameldinmostafamohamedahmed@gmail.com>
2023-06-21 11:56:43 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,10 @@ int main(void)
 
 	r = _islower('H');
 	_putchar(r + 'O');
-
 	r = _islower(r + 'o');
 	_putchar(r + '0');
-	r = _islower('108');
+	r = _islower(108);
 	_putchar(r + '0');
-	_putchar('\n');											}
+	_putchar('\n');
+	return (0);
+}

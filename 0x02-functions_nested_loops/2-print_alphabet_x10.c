bf16800fc0d1ea873e177c5648cd23d8e64ba521
Hossam Eldin Mostafa Mohamed <hossameldinmostafamohamedahmed@gmail.com>
2023-06-21 07:04:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar (j);
-		}
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+	_putchar (j);
+	}
 
 	}
 	 _putchar (\n);
